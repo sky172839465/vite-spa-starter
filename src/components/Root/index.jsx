4f3d5fb650fb612d7a83277f3932ae6e2b3e1fc4
sky172839465 <sky172839465@gmail.com>
2024-09-10 00:04:34 +0800

    feat: add request error handle

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,10 +1,28 @@
-import { Toaster } from 'react-hot-toast'
+import { get } from 'lodash-es'
+import { useRef } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 import { SWRConfig } from 'swr'
 
 import fetcher from '../../utils/fetcher'
 
 const Root = (props) => {
   const { children } = props
+  const errorToastIdRef = useRef()
+  const errorToastKeyRef = useRef()
+
+  const onError = (error, key) => {
+    errorToastIdRef.current = key
+    console.log(error)
+    errorToastKeyRef.current = toast.error(get(error, 'message', 'error'))
+  }
+
+  const onSuccess = (data, key) => {
+    if (key !== errorToastKeyRef.current) {
+      return
+    }
+    toast.dismiss(errorToastIdRef.current)
+  }
+
   return (
     <>
       <SWRConfig
@@ -14,7 +32,9 @@ const Root = (props) => {
           keepPreviousData: true,
           errorRetryCount: 3,
           suspense: false,
-          fetcher
+          fetcher,
+          onError,
+          onSuccess
         }}
       >
         {children}
