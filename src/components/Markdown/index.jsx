ba5ed95a787d9f1942e8ce8d879ecda4f516c0f9
sky172839465 <sky172839465@gmail.com>
2024-12-01 18:46:27 +0800

    feat: add github edit link

--- src/components/Markdown/index.jsx
+++ src/components/Markdown/index.jsx
@@ -3,10 +3,26 @@ import 'github-markdown-css/github-markdown.css'
 import { useAsyncValue } from 'react-router-dom'
 
 const Markdown = (props) => {
+  const { filePath } = props
   const { html: __html, attributes } = useAsyncValue()
+  const { title } = attributes
   console.log(props, attributes)
   return (
-    <div>
+    <div className='space-y-2'>
+      <div className='flex flex-row items-center justify-between'>
+        <h2 className='text-xl'>
+          {title}
+        </h2>
+        <div>
+          <a
+            href={`https://github.com/sky172839465/vite-spa-starter/edit/main/src/${filePath.replace('./', '')}`}
+            target='_blank'
+            className='btn btn-primary'
+          >
+            Edit this page on GitHub
+          </a>
+        </div>
+      </div>
       {/* eslint-disable-next-line tailwindcss/no-custom-classname */}
       <article className='markdown-body'>
         <div
