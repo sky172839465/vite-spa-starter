2e948fbe86a341a21f19e808b609ed0ea5c53ddf
sky172839465 <sky172839465@gmail.com>
2024-12-10 18:47:14 +0800

    fix: scroll top not top to page

--- src/components/BottomActions/ScrollToTop/index.jsx
+++ src/components/BottomActions/ScrollToTop/index.jsx
@@ -3,7 +3,7 @@ import { GoMoveToTop } from 'react-icons/go'
 const ScrollToTop = (props) => {
   const { topRef } = props
   const scrollToTop = () => {
-    topRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    topRef.current.scrollIntoView({ behavior: 'smooth', block: 'center' })
   }
 
   return (
